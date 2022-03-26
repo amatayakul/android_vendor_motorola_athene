f5b122cafbfaa4d67c96271d268ecd905f335b22
TheGhostReborn <abhinandammanamkandy@gmail.com>
2022-03-26 18:14:55 +0530

    athene: DT_NEEDED fix

--- Android.mk
+++ Android.mk
@@ -46,6 +46,7 @@ LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_SHARED_LIBRARIES := android.hidl.base@1.0 libc++ libc libcutils libdl libhidlbase libhidltransport libhwbinder liblog libm libutils
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
